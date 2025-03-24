38349d3b779c7e1f2d1e5d5cb813f08df9d5a155
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 19:05:40 -0400

    new program

---
+++ src/MyImportingClass.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class MyImportingClass {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list);
+    }
+}
