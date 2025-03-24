81b1feef521772745a783a080901263fc2d5acf0
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 14:53:46 -0400

    example code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    // 1.1: Understanding Scope
+    int num1 = 0;           // class-level variable's scope example
 
-    /*hello */
-    //yes
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int num2 = 0;               //method-level variable's scope example
+        System.out.println(num2);
+
+        {
+            int num3 = 0;                   //block-level variable's scope example
+            System.out.println(num3);       // will only be printed if called within this code block
+        }
+    }
     
 }
