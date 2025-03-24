fa2a27c41a9ec154b1ff32c53de5571cc03d5faf
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 19:48:50 -0400

    encap

---
+++ src/encapsulationClass.java
@@ -0,0 +1,11 @@
+public class encapsulationClass {
+    private String myField;
+    
+    public String getMyField() {
+        return myField;
+    }
+
+    public void setMyField(String myField) {
+        this.myField = myField;
+    }
+}
