2bd94745b90a7e5741967a3bb599dd5cb2b619a7
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 19:33:54 -0400

    inheritance class example

---
+++ src/inheritance.java
@@ -0,0 +1,18 @@
+public class inheritance {
+    protected String myField;
+
+    protected void myMethod() {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+
+Inheritance Demo:
+
+* New class that simply inherits all the methods and fields of the superclass, 
+no additional functionality yet. Will not actually be declaring it, since I don't need it
+
+public class inheritSubclass extends inheritance{}
+
+*/
