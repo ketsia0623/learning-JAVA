79ed376fec5c793e693fec25a76b7c707c8eb28d
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 19:44:53 -0400

    abstraction

--- src/abstractionClass.java
+++ src/abstractionClass.java
@@ -1,3 +1,16 @@
+// abstract classes and methods use the abstract keyword
+
 public abstract class abstractionClass {
-    
+    public abstract void myMethod();
 }
+
+
+/*
+public class abstractionSubClass etends abstractionClass {
+    // not the same myMethod() from the superclass    
+    public void myMethod() {
+        System.out.println("Hello World!");
+    }
+}
+  
+*/
