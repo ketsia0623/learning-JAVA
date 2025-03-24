7badca9dd895e67728feb6ecd1e70ed910d9dec9
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-24 19:38:12 -0400

    poly

---
+++ src/polymorphism.java
@@ -0,0 +1,28 @@
+public class polymorphism {
+    protected String myField;
+
+    protected void myMethod() {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+
+Polymorphism Demo:
+
+* the body of the methods of the subclasses are different than that of it's parent's method 
+
+public class polySubclass1 extends polymorphism{
+    protected void myMethod() {
+        System.out.println("Hello World 1!");
+    }
+}
+
+
+public class polySubclass2 extends polymorphism {
+    protected void myMethod() {
+        System.out.println("Hello World 2!");
+    }
+}
+
+*/
