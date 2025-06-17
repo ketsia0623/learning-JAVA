f80b3798ca9c96ce12e20790e69dab25c59321b7
ketsia0623 <ketsiad@udel.edu>
2025-06-17 15:24:01 -0400

    break coding practice

---
+++ src/BreakPractice.java
@@ -0,0 +1,17 @@
+public class BreakPractice {
+    public static void main(String[] args) {
+        int i, j;
+    newloop: for (i = 1; i < 4; i++) {
+        j = 1;
+        while (j < 4) {
+            if (j % 2 == 0) {
+                break newloop;      // exits the outer for-loop entirely
+            }
+            j++;
+            System.out.println("inner");
+        }
+        System.out.println("outer");
+    }
+
+    }
+}
