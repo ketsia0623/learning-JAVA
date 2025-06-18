8db1748a787cb9c124e106f024c6f51f7a7bedbd
ketsia0623 <ketsiad@udel.edu>
2025-06-18 15:45:16 -0400

    b n c code

--- src/BreakPractice.java
+++ src/BreakPractice.java
@@ -1,11 +1,13 @@
 public class BreakPractice {
     public static void main(String[] args) {
         int i, j;
-    newloop: for (i = 1; i < 4; i++) {
+    //newloop: for (i = 1; i < 4; i++) {
+    for (i = 1; i < 4; i++) {
         j = 1;
         while (j < 4) {
             if (j % 2 == 0) {
-                break newloop;      // exits the outer for-loop entirely
+                //break newloop;      // exits the outer for-loop entirely
+                break;
             }
             j++;
             System.out.println("inner");
--- src/ContinuePractice.java
+++ src/ContinuePractice.java
@@ -2,10 +2,12 @@ public class ContinuePractice {
     public static void main(String[] args) {
         int i, j;
     newloop: for (i = 1; i < 4; i++) {
+    //for (i = 1; i < 4; i++) {
         j = 1;
         while (j < 4) {
             if (j % 2 == 0) {
-                continue newloop;      // exits the outer for-loop entirely
+                continue newloop;      // skips the outer for-loop entirely
+                //continue;
             }
             j++;
             System.out.println("inner");
